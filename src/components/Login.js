71c7e6032954d066db159e0fc7b9273eea30d29e
Naveen <naveengali80@gmail.com>
2021-07-11 11:56:24 +0530

    Initialised app and started Login page

---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../Styles/Login.css";
+
+function Login() {
+  return (
+    <div className="login">
+      <h2>I am the login page</h2>
+      {/* Spotify Logo */}
+      {/* Login with Spotify Button */}
+    </div>
+  );
+}
+
+export default Login;
