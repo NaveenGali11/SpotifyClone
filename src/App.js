9da9af6fa3098f7bb1b868ab05915355046c11d7
Naveen <naveengali80@gmail.com>
2021-07-11 15:06:34 +0530

    Got access_token from the url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./components/Login";
+import { getTokenFromUrl } from "./components/spotify";
 
 function App() {
+  const [state, setstate] = useState(initialState);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+
+    const token = hash.access_token;
+
+    console.log("I have a token ---> ", token);
+  }, []);
+
   return (
     <div className="App">
       <Login />
